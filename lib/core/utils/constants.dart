c1a91fd344a8a11114fcaa339432ff722200057e
ahmed <ahmedeltabakh703@gmail.com>
2025-02-07 17:10:38 +0200

    home_screen

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color.fromRGBO(237, 233, 255, 1);
 const buttonPrimaryColor = Color.fromRGBO(10, 12, 60, 1);
+const bottomNavigationBar = Color.fromARGB(255, 13, 71, 161);
 const kSecondryColor = Color.fromRGBO(255, 255, 255, 1);
 final double appBarHeight = AppBar().preferredSize.height;
 var profileImage;
